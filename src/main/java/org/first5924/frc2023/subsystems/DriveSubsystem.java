e03bf6db6f012529927f1fe379a93d883a49c27e
Chris Voon <christopher.voon@gmail.com>
2023-01-13 17:18:01 -0800

    Remove unused import

--- src/main/java/org/first5924/frc2023/subsystems/DriveSubsystem.java
+++ src/main/java/org/first5924/frc2023/subsystems/DriveSubsystem.java
@@ -14,7 +14,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
