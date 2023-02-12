2d711ae35126550f03df863b1ace42a4ae95d9a6
Chris Voon <christopher.voon@gmail.com>
2023-02-11 16:57:01 -0800

    Use normal RamseteCommand with path following

--- src/main/java/org/first5924/frc2023/commands/autonomous/DriveOneMeter.java
+++ src/main/java/org/first5924/frc2023/commands/autonomous/DriveOneMeter.java
@@ -8,20 +8,20 @@ import org.first5924.frc2023.constants.DriveConstants;
 import org.first5924.frc2023.subsystems.drive.DriveSubsystem;
 
 import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.commands.PPRamseteCommand;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class DriveOneMeter extends SequentialCommandGroup {
-  PathPlannerTrajectory mOneMeter = PathPlanner.loadPath("One Meter", 2.5, 2);
+  Trajectory mOneMeter = PathPlanner.loadPath("One Meter", 2.5, 2);
 
   /** Creates a new DriveOneMeter. */
   public DriveOneMeter(DriveSubsystem drive) {
@@ -29,20 +29,20 @@ public class DriveOneMeter extends SequentialCommandGroup {
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       new InstantCommand(() -> {
-        drive.resetPosition(null);
-      })
-      // ,
-      // new PPRamseteCommand(
-      //   mOneMeter,
-      //   drive::getEstimatedRobotPose,
-      //   new RamseteController(),
-      //   new SimpleMotorFeedforward(DriveConstants.ks, DriveConstants.kv, DriveConstants.ka),
-      //   DriveConstants.kKinematics,
-      //   drive::getWheelSpeeds,
-      //   new PIDController(DriveConstants.kP, DriveConstants.kI, DriveConstants.kD),
-      //   new PIDController(DriveConstants.kP, DriveConstants.kI, DriveConstants.kD),
-      //   drive::voltageDrive,
-      //   drive)
+        drive.resetPosition(mOneMeter.getInitialPose());
+      }),
+      new RamseteCommand(
+        mOneMeter,
+        drive::getEstimatedRobotPose,
+        new RamseteController(),
+        new SimpleMotorFeedforward(DriveConstants.ks, DriveConstants.kv, DriveConstants.ka),
+        DriveConstants.kKinematics,
+        drive::getWheelSpeeds,
+        new PIDController(DriveConstants.kP, DriveConstants.kI, DriveConstants.kD),
+        new PIDController(DriveConstants.kP, DriveConstants.kI, DriveConstants.kD),
+        drive::driveVoltage,
+        drive
+      )
     );
   }
 }
