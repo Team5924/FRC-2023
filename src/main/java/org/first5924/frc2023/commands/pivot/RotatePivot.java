5d8473651abadd7ec69f74baa3dec78bd0425d0d
Chris Voon <christopher.voon@gmail.com>
2023-03-04 13:39:25 -0800

    Fix pivot manual control direction

--- src/main/java/org/first5924/frc2023/commands/pivot/RotatePivot.java
+++ src/main/java/org/first5924/frc2023/commands/pivot/RotatePivot.java
@@ -35,7 +35,7 @@ public class RotatePivot extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    mPivot.setPercent(MathUtil.applyDeadband(mJoystickY.getAsDouble(), OIConstants.kOperatorJoystickDeadband));
+    mPivot.setPercent(MathUtil.applyDeadband(-mJoystickY.getAsDouble(), OIConstants.kOperatorJoystickDeadband) * 0.3);
   }
 
   // Called once the command ends or is interrupted.
